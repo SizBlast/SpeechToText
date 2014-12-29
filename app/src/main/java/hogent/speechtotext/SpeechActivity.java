5d26e1e126339a8704a61f96fb59be86087f2788
Christof Van Cauteren <christofvc@gmail.com>
2014-12-29 18:10:12 +0100

    Finished task 2 and 3

--- app/src/main/java/hogent/speechtotext/SpeechActivity.java
+++ app/src/main/java/hogent/speechtotext/SpeechActivity.java
@@ -1,19 +1,62 @@
 package hogent.speechtotext;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.TextView;
+import java.util.ArrayList;
 
 public class SpeechActivity extends ActionBarActivity {
+
+    private static final int SHOW_SUBACTIVITY = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_speech);
+
+        View goButton = findViewById(R.id.go_btn);
+        goButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                    i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                    i.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak...");
+                    startActivityForResult(i, SHOW_SUBACTIVITY);
+                }
+                catch (ActivityNotFoundException e) {
+                    TextView result_txt = (TextView) findViewById(R.id.result_txt);
+                    result_txt.setText("Voice recognition not supported.");
+                    Log.e("onCreate","Voice recognition not supported.");
+                }
+            }
+        });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SHOW_SUBACTIVITY && resultCode == RESULT_OK) {
+            TextView result_txt = (TextView) findViewById(R.id.result_txt);
+            ArrayList<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            if (results != null) {
+                result_txt.setText(results.get(0));
+            }
+            else {
+                Log.e("onActivityResult", "Results empty");
+            }
+        }
+        else {
+            Log.e("onActivityResult", "Not ok");
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
